78d2ca166da20cd981d6e5186489971a2ed874cd
O1enb <vnmiraev@gmail.com>
2024-09-11 18:35:46 +0300

    Неизменяемые и изменяемые объекты. Кортежи

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+
+my_dict = {'x': 10, 'y': 20, 'z': True}
+print(my_dict)
+
+print(my_dict['y'])
+print(my_dict.get('h'))
+
+my_dict.update({'g': 3000, 't': 149})
+print(my_dict.pop('z'))
+print(my_dict)
+
+my_set = {1, 3 , 3, False, 'Aye', 1}
+print(my_set)
+
+my_set.discard(3)
+print(my_set)
