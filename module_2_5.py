35db38f7b0f06a2b86e371a6adda0a3544bd9325
O1enb <vnmiraev@gmail.com>
2024-09-20 14:20:13 +0300

    Функции в Python.Функция с параметром

--- module_2_5.py
+++ module_2_5.py
@@ -1,5 +1,7 @@
 def get_matrix (n, m, value):
     matrix = []
+    if m == 0 or n == 0:
+        return matrix
     for x in range(n):
         row = [value] * m
         matrix.append(row)
@@ -15,8 +17,12 @@ result1 = get_matrix(2,2,10)
 result2 = get_matrix(3,5,42)
 result3 = get_matrix(4,2,13)
 
-print_matrix(result1)
-print('')
-print_matrix(result2)
-print('')
-print_matrix(result3)
+print(result1)
+print(result2)
+print(result3)
+
+#print_matrix(result1)
+#print('')
+#print_matrix(result2)
+#print('')
+#print_matrix(result3)
