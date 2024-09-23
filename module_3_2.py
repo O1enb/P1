9b4421aa41c3179a0b70bd5878e30ca7e9004724
O1enb <vnmiraev@gmail.com>
2024-09-23 15:04:31 +0300

    Способы вызова функции

---
+++ module_3_2.py
@@ -0,0 +1,17 @@
+
+
+def send_email(mes, rec, *, sen = 'university.help@gmail.com'):
+    if '@' not in rec or '@' not in sen:
+        print(f"Невозможно отправить письмо с адреса {sen} на адрес {rec}.")
+    elif sen == rec:
+        print("Нельзя отправить письмо самому себе!")
+    elif sen == 'university.help@gmail.com':
+        print(f"Письмо успешно отправлено с адреса {sen} на адрес {rec}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sen} на адрес {rec}.")
+
+
+send_email("message", 'university.helpgmail.com')
+send_email("message", 'university.help@gmail.com')
+send_email("message", 'university@gmail.com')
+send_email("message", 'universityp@gmail.com', sen = 'help@gmail.com')
