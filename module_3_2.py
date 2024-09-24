7ebd62c4710a3ad7a2d1466800586deeb93086c9
O1enb <vnmiraev@gmail.com>
2024-09-24 13:22:48 +0300

    Способы вызова функции

--- module_3_2.py
+++ module_3_2.py
@@ -1,7 +1,7 @@
 
 
 def send_email(mes, rec, *, sen = 'university.help@gmail.com'):
-    if '@' not in rec or '@' not in sen:
+    if '@' not in rec or '@' not in sen or not rec.endswith((".com",".ru",".net")) or not sen.endswith((".com",".ru",".net")):
         print(f"Невозможно отправить письмо с адреса {sen} на адрес {rec}.")
     elif sen == rec:
         print("Нельзя отправить письмо самому себе!")
