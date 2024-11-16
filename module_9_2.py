9d4d9e33d5b908b3db1fe5700a3cd8b1e37015fa
O1enb <vnmiraev@gmail.com>
2024-11-16 09:13:42 +0300

    Списковые, словарные сборки

--- module_9_2.py
+++ module_9_2.py
@@ -5,7 +5,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 first_result = [len(x) for x in first_strings if len(x) >= 5]
 print(first_result)
 
-second_result = [(first_strings[i], second_strings[i]) for i in range(min(len(first_strings), len(second_strings)))]
+second_result = [(x,y) for x in first_strings for y in second_strings if len(x) == len(y)]
 print(second_result)
 
 third_result = {string: len(string) for i in range(min(len(first_strings), len(second_strings))) for string in (first_strings[i], second_strings[i]) if len(first_strings) % 2}
